ce2519774835274411de2db5d91fa707abfe3bcc
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-10-11 22:51:01 +0400

    initial react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./favicon.ico";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TurdakovA</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="full-screen">
+        <div class="full-screen__body">
+          <div class="full-screen__title">Turdakov Alexey</div>
+          <div class="full-screen__text">
+            Lorem ipsum dolor sit amet consectetur
+          </div>
+          <div class="full-screen__icon">
+            
+          </div>
+        </div>
+        <video autoplay muted loop preload="auto" class="full-screen__video">
+          <source type="video/webm" src="./video/water.webm" />
+          <source type="video/mp4" src="./video/water.mp4" />
+        </video>
+      </div>
     </div>
+  </body>
+</html>
+
   );
 }
 
